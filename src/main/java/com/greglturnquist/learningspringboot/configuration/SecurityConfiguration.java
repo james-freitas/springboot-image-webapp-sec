0fad12c9d4d824e0979016cd7d89f59d468a011a
James Daniel Correia de Freitas <jamesfrj@yahoo.com.br>
2016-12-26 20:54:27 -0200

    First commit

---
+++ src/main/java/com/greglturnquist/learningspringboot/configuration/SecurityConfiguration.java
@@ -0,0 +1,43 @@
+
+package com.greglturnquist.learningspringboot.configuration;
+
+import com.greglturnquist.learningspringboot.service.SpringDataUserDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * @author Greg Turnquist
+ */
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+			.authorizeRequests()
+				.antMatchers(HttpMethod.GET, "/", "/images/**", "/main.css", "/webjars/**").permitAll()
+				.antMatchers(HttpMethod.POST, "/images").hasRole("USER")
+				.antMatchers("/imageMessages/**").permitAll()
+				.and()
+			.formLogin()
+				.permitAll()
+				.and()
+			.logout()
+				.logoutSuccessUrl("/");
+	}
+
+	@Autowired
+	public void configureUsers(AuthenticationManagerBuilder auth,
+							   SpringDataUserDetailsService userDetailsService) throws Exception {
+		auth.userDetailsService(userDetailsService);
+	}
+
+}
